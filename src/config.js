cdcb6cfcae567ffa9854d153f752fe575c9dc921
Alessandro Nadalin <alessandro.nadalin@gmail.com>
2015-08-05 12:29:07 +0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,7 +6,15 @@ var argv          = require('yargs').argv;
 var _             = require('lodash');
 var url           = require('url');
 var logger        = require('./logger');
-var userConfig    = yaml.safeLoad(fs.readFileSync(path.join(argv.config), 'utf8'));
+var userConfig    = {};
+
+try {
+ var userConfig    = yaml.safeLoad(fs.readFileSync(path.join(argv.config), 'utf8')); 
+} catch (err) {
+  logger.info('Unable to find config file, proceeding with a bare minimum configuration');
+  logger.info('You might want to fix this unless you are passing config values through environment variables (ROGER_CONFIG_...)');
+}
+
 var config        = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '..', 'config', 'base.yml'), 'utf8'));
 
 _.assign(config, userConfig);
