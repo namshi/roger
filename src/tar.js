d0192d1a66057e62dcf52c9be5cbc2671b7636a2
odino <alessandro.nadalin@gmail.com>
2016-08-14 16:19:53 +0400

    Replaced the Node `tar` implementation with the linux command

--- src/tar.js
+++ src/tar.js
@@ -1,55 +1,33 @@
-var fs      = require('fs')
-var baseTar = require('tar-fs')
-var logger  = require('./logger');
 var Q       = require('q');
+var spawn   = require('child_process').spawn;
 
 var tar = {};
 
 /**
- * Creates a tarball from a readable stream,
- * at the given path.
- * 
- * @return promise
- */
-tar.createFromStream = function(path, stream) {
-  stream.pipe(baseTar.extract(path));
-  
-  return Q.Promise(function(resolve, reject){
-    stream.on('error', function(err){
-      reject(err);
-    })
-  
-    stream.on('end', function(){
-      resolve();
-    });
-  });
-}
-
-/**
  * Creates a tarball at path with the contents
  * of the sourceDirectory.
- * 
+ *
  * After the tarball is created, the callback
  * gets invoked.
- * 
+ *
  * @return promise
  */
 tar.create = function(path, sourceDirectory, cb){
-  var readable = fs.createWriteStream(path);
   var deferred = Q.defer();
+  var tar = spawn('tar', ['-C', sourceDirectory, '-czvf', path, '.']);
 
-  baseTar.pack(sourceDirectory).pipe(readable).on('error', function(err){
-    deferred.reject(err);
+  tar.stderr.on('data', function(data) {
+    deferred.reject(data)
   });
 
-  readable.on('error', function(err){
-    deferred.reject(err);
-  })
-  
-  readable.on('finish', function(){
-    deferred.resolve();
+  tar.on('close', function(code) {
+    if (code === 0) {
+      deferred.resolve()
+    } else {
+      deferred.reject(new Error("Unable to tar -- exit code " + code))
+    }
   });
-  
+
   return deferred.promise;
 };
 
