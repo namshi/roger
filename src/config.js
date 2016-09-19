869b9a479cc5a85ca9fa97928f9bf6d0161c0575
odino <alessandro.nadalin@gmail.com>
2016-09-19 21:17:52 +0400

    Added more logs to the new tar implementation, fixed as it would get stuck as no one was consuming the stream

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ var logger        = require('./logger');
 var userConfig    = {};
 
 try {
- var userConfig    = yaml.safeLoad(fs.readFileSync(path.join(argv.config), 'utf8')); 
+ var userConfig    = yaml.safeLoad(fs.readFileSync(path.join(argv.config), 'utf8'));
 } catch (err) {
   logger.info('Unable to find config file, proceeding with a bare minimum configuration');
   logger.info('You might want to fix this unless you are passing config values through environment variables (ROGER_CONFIG_...)');
--- src/tar.js
+++ src/tar.js
@@ -12,18 +12,27 @@ var tar = {};
  *
  * @return promise
  */
-tar.create = function(path, sourceDirectory, cb){
+tar.create = function(path, sourceDirectory, buildLogger, buildOptions){
   var deferred = Q.defer();
   var tar = spawn('tar', ['-C', sourceDirectory, '-czvf', path, '.']);
 
+  buildLogger.info('[%s] Creating tarball', buildOptions.buildId);
+
   tar.stderr.on('data', function(data) {
+    buildLogger.error('[%s] Error creating tarball', buildOptions.buildId);
     deferred.reject(data)
+  });
+
+  tar.stdout.on('data', function(data) {
+    buildLogger.error('[%s] %s', buildOptions.buildId, data.toString());
   });
 
   tar.on('close', function(code) {
     if (code === 0) {
+      buildLogger.info('[%s] Tarball created', buildOptions.buildId);
       deferred.resolve()
     } else {
+      buildLogger.info('[%s] Error creating tarball', buildOptions.buildId);
       deferred.reject(new Error("Unable to tar -- exit code " + code))
     }
   });
